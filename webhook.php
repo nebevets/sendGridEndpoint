76769a3bf08ec4d61380b13e9c3879d565f5faa4
nebevets <steveben@exmsft.com>
2018-10-04 18:52:48 -0700

    added error log

--- webhook.php
+++ webhook.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('credentials.php');
+error_log('message*$*$*$*$*$*$*$*$*'.print_r($_POST, true));
 
 $query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['html']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
