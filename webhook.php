c34d90535a2100da6be11f1c843e173fe7a907de
nebevets <steveben@exmsft.com>
2018-10-04 19:02:21 -0700

    added slashes'

--- webhook.php
+++ webhook.php
@@ -3,6 +3,8 @@
 require_once('credentials.php');
 error_log('message*$*$*$*$*$*$*$*$*'.print_r($_POST, true));
 
+$_POST['html']=addslashes($_POST['html']);
+
 $query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['html']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
 error_log('messageYYYYYYY '.$query);
