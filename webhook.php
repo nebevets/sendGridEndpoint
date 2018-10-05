cc8226d8dcce00e63204c839f50c4f6f664a82a0
nebevets <steveben@exmsft.com>
2018-10-04 18:30:03 -0700

    modified query and connection

--- webhook.php
+++ webhook.php
@@ -2,8 +2,8 @@
 
 require_once('credentials.php');
 
-$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['email']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
+$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['html']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
-mysqli_query($conn, $query);
+mysqli_query($connection, $query);
 
 ?>
