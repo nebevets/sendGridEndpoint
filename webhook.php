fca91a88d31da7a922da9a79b8ab53772e8be588
nebevets <steveben@exmsft.com>
2018-10-15 10:49:17 -0700

    added htmlSource

--- webhook.php
+++ webhook.php
@@ -1,11 +1,14 @@
 <?php
 
 require_once('credentials.php');
-error_log('message*$*$*$*$*$*$*$*$*'.print_r($_POST, true));
+error_log('squirrelly'.print_r($_POST, true));
 
-$_POST['html']=addslashes($_POST['html']);
+//$_POST['html']=addslashes($_POST['html']);
 
-$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['text']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
+$query = `INSERT INTO receivedEmail SET subject='{$_POST['subject']}',
+          emailText='{$_POST['text']}', htmlSource='{$_POST['html']}', 
+          sender='{$_POST['from']}',
+          recipient='{$_POST['to']}', sent=NOW()`;
 
 error_log('messageYYYYYYY '.$query);
 mysqli_query($connection, $query);
