3cb1eeb8d9b331d65db781180b36a2f9bb562c5d
nebevets <steveben@exmsft.com>
2018-10-15 10:41:22 -0700

    changed body to text for test

--- webhook.php
+++ webhook.php
@@ -5,7 +5,7 @@ error_log('message*$*$*$*$*$*$*$*$*'.print_r($_POST, true));
 
 $_POST['html']=addslashes($_POST['html']);
 
-$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['html']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
+$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['text']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
 error_log('messageYYYYYYY '.$query);
 mysqli_query($connection, $query);
