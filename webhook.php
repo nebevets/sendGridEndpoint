4d01fcea6f4b93f55161bb3a546f05adf875749d
nebevets <steveben@exmsft.com>
2018-10-15 10:58:56 -0700

    added htmlSource

--- webhook.php
+++ webhook.php
@@ -5,10 +5,7 @@ error_log('squirrelly'.print_r($_POST, true));
 
 $_POST['html']=addslashes($_POST['html']);
 
-$query = `INSERT INTO receivedEmail SET subject='{$_POST['subject']}',
-          emailText='{$_POST['text']}', htmlSource='{$_POST['html']}', 
-          sender='{$_POST['from']}',
-          recipient='{$_POST['to']}', sent=NOW()`;
+$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', emailText='{$_POST['text']}', htmlSource='{$_POST['html']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
 error_log('messageYYYYYYY '.$query);
 mysqli_query($connection, $query);
