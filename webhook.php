1f99631ea47deba80d0c3e07dbdf64eef3709b42
nebevets <steveben@exmsft.com>
2018-10-18 18:25:05 -0700

    implemented regex for square in php

--- webhook.php
+++ webhook.php
@@ -4,7 +4,7 @@ error_log('squirrellyPost'.print_r($_POST, true));
 
 
 $_POST['html']=addslashes($_POST['html']);
-preg_match('/Receipt\sfor\s(\$\d+.\d+)\sat\s([a-zA-Z\-\&_@\s]+)\son\s(.*)\sat/', $_POST['html'], $receiptMatches, PREG_OFFSET_CAPTURE);
+preg_match('/eceipt\s[form]+\s(\$[,.\d]+)\sat\s(.*)\son\s([A-Za-z\s\d]+)\sat/', $_POST['html'], $receiptMatches, PREG_OFFSET_CAPTURE);
 preg_match('/([0-9a-zA-Z_\.]+)@/', $_POST['to'], $userMatch, PREG_OFFSET_CAPTURE);
 if (count($receiptMatches)){
     $working = 1;
