da846667bea1c5c12e64d97935d2a89a4ca80712
nebevets <steveben@exmsft.com>
2018-10-18 17:51:48 -0700

    implemented regex for square in php

--- webhook.php
+++ webhook.php
@@ -1,13 +1,32 @@
 <?php
-
 require_once('credentials.php');
-error_log('squirrelly'.print_r($_POST, true));
+error_log('squirrellyPost'.print_r($_POST, true));
+
 
 $_POST['html']=addslashes($_POST['html']);
+preg_match('/Receipt\sfor\s(\$\d+.\d+)\sat\s([a-zA-Z\-\&_@\s]+)\son\s(.*)\sat/', $_POST['html'], $receiptMatches, PREG_OFFSET_CAPTURE);
+preg_match('/([0-9a-zA-Z_\.]+)@/', $_POST['to'], $userMatch, PREG_OFFSET_CAPTURE);
+if (count($receiptMatches)){
+    $working = 1;
+}else{
+    $working = 0;
+}
 
-$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', emailText='{$_POST['text']}', htmlSource='{$_POST['html']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
+$query = "INSERT INTO receivedEmail
+            SET
+            subject='{$_POST['subject']}',
+            emailText='{$_POST['text']}',
+            htmlSource='{$_POST['html']}',
+            sender='{$_POST['from']}',
+            userName='{$userMatch[1][0]}',
+            sent=NOW(),
+            total='{$receiptMatches[1][0]}',
+            storeName='{$receiptMatches[2][0]}',
+            date='{$receiptMatches[3][0]}',
+            workingRegEx='{$working}'";
 
-error_log('messageYYYYYYY '.$query);
+error_log('squirrellyQuery '.$query);
 mysqli_query($connection, $query);
 
+echo $query;
 ?>
