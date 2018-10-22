acbfa34e243ed300e33135436cde51d2bf222677
nebevets <steveben@exmsft.com>
2018-10-22 15:57:59 -0700

    fixed userId issue

--- webhook.php
+++ webhook.php
@@ -3,7 +3,7 @@ require_once('credentials.php');
 error_log('squirrellyPost'.print_r($_POST, true));
 
 
-$_POST['html']=htmlentities(addslashes($_POST['html']));
+$_POST['html']=addslashes($_POST['html']);
 
 preg_match('/eceipt\s[form]+\s\$([,.\d]+)\sat\s(.*)\son\s([A-Za-z\s\d]+)\sat/', $_POST['html'], $receiptMatches, PREG_OFFSET_CAPTURE);
 preg_match('/([0-9a-zA-Z_\.]+)@/', $_POST['to'], $userMatch, PREG_OFFSET_CAPTURE);
